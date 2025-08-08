02975a4f0d0734115c825b48a28b94e70e694351
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-08 20:18:10 +0300

    1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,10 @@
 const keyboarddDiv = document.querySelector(".keyboard");
 
+
+//Creating keyboard buttons 
 for (let i= 97; i<= 122; i++ ){
     const button = document.createElement("button");
     button.innerText=String.fromCharCode(i); 
-    
+    keyboarddDiv.appendChild(button);
 
 }
