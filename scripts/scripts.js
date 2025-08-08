8ab1d28fae915a2773565fc1b9193115cba24307
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-08 20:38:26 +0300

    1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,17 @@
-const keyboarddDiv = document.querySelector(".keyboard");
+const keyboardDiv = document.querySelector(".keyboard");
 
 const getRandomWord = () =>{
-    const {word , hint }= wordList[Math.floor(Math.random() * wordList.length)]
+    // Selecting a random word and hint from the wordList
+    const {word , hint }= wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word, hint)
 }
 
 //Creating keyboard buttons 
 for (let i= 97; i<= 122; i++ ){
     const button = document.createElement("button");
     button.innerText=String.fromCharCode(i); 
-    keyboarddDiv.appendChild(button);
+    keyboardDiv.appendChild(button);
 
 }
+
+getRandomWord();
