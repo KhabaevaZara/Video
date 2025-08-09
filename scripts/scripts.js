2da005616bbbc3cb4b9ec27730a09071829e31a0
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-09 12:39:48 +0300

    1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,24 +3,35 @@ const wordDisplay = document.querySelector(".word-display");
 const guessesText = document.querySelector(".guesses-text b");
 const keyboardDiv = document.querySelector(".keyboard");
 const gameModal = document.querySelector(".game-modal");
-let currentWord, correctLetters =[ ],  wrongGuessCount =0;
+const playAgainBtn = document.querySelector(".play-again")
+let currentWord, correctLetters ,  wrongGuessCount;
 const maxGuesses = 6;
+const resetGame =() =>{
+    correctLetters =[ ];
+    wrongGuessCount =0;
+    hangmanImage.src=`images/hangman-${wrongGuessCount}.svg`;
+    guessesText.innerText = `${wrongGuessCount}/${maxGuesses}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false );
+    wordDisplay.innerHTML = word.split("").map( () => `<li class="letter "></li>`).join("");
+    gameModal.classList.remove("show");
+}
 
 const getRandomWord = () =>{
     // Selecting a random word and hint from the wordList
     const {word , hint }= wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
     console.log(word)
-    
     document.querySelector(".hint-text b").innerText = hint;
-    wordDisplay.innerHTML = word.split("").map( () => `<li class="letter "></li>`).join("");
+    resetGame();
+   
 }
 const gameOver =(isVictory) => {
+    //After 600ms of game  complete... showing modal with relevant details
     setTimeout(() => {
         const modalText = isVictory ? `You found the word:`: `The correct word was : `;
         gameModal.querySelector("img").src = `images/${ isVictory ? 'victory' : 'lost'}.gif`;
         gameModal.querySelector("h6").innerText = `${ isVictory ? 'Congrats!' : 'Game Over!'}`;
-        gameModal.querySelector("p").innerHTML = `${modalText} <b></b>`;
+        gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
         gameModal.classList.add("show");
     }, 300 );
 };
@@ -58,3 +69,4 @@ for (let i= 97; i<= 122; i++ ){
 }
 
 getRandomWord();
+playAgainBtn.addEventListener("click",getRandomWord );
